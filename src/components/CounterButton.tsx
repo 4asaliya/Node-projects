5f7e0c28e982d300bd5a3feb12745db55c9a3a34
Your Name <you@example.com>
2026-02-13 00:03:31 +0600

    propssss

---
+++ src/components/CounterButton.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Button, Text, View, StyleSheet } from "react-native";
+
+type CounterButton = {
+  title: string;
+  initialValue: number;
+  step: number;
+};
+
+export default function CounterButton({ title, initialValue, step }:CounterButton) {
+  const [count, setCount] = useState(initialValue);
+
+  return (
+    <View style={styles.container}>
+      <Text>{count}</Text>
+      <Button
+        title={title}
+        onPress={() => setCount(count + step)}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    marginVertical: 10,
+    flex: 1,
+    marginRight: 8,
+  },
+});
